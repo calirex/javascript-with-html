c48ebb25325dc074b70afa0ecc99f5ef1fe87ae2
Fernando Rivera <fernando.rivera0011@gmail.com>
2022-11-21 11:46:24 -0600

    finished-js-with-html exercise

---
+++ js/external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+let usercolor = prompt('What Is your favorite color?');
+alert("Great " + usercolor + " is my favorite color too!");
